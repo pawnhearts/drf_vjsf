80acc00f221f8eb72f045230c05719fec61077a7
ph <robotnaoborot@gmail.com>
2021-02-04 00:11:38 +0300

    moar

--- drf_vjsf/__init__.py
+++ drf_vjsf/__init__.py
@@ -1,15 +1,22 @@
-from rest_framework.reverse import reverse
-
-from drf_jsonschema import to_jsonschema
+from drf_jsonschema import converters
+from .converters import PrimaryKeyRelatedFieldConverter, DecimalFieldConverter
 from rest_framework.metadata import SimpleMetadata
+
+
+# Yeah, I know :-/
+converters.PrimaryKeyRelatedFieldConverter = PrimaryKeyRelatedFieldConverter
+converters.DecimalFieldConverter = DecimalFieldConverter
 
 
 class JsonSchemaMetadata(SimpleMetadata):
 
     def determine_metadata(self, request, view):
+        from drf_jsonschema import to_jsonschema
+
         metadata = super().determine_metadata(request, view)
-        metadata['schema'] = to_jsonschema(view.get_serializer())
-        metadata['url'] = view.request.path
-        metadata['detail'] = view.detail
+        if serializer := getattr(view, 'get_serializer', getattr(view, 'serializer_class', None)):
+            metadata['schema'] = to_jsonschema(serializer)
+            metadata['url'] = view.request.path
+            metadata['detail'] = view.detail
 
         return metadata
---
+++ drf_vjsf/converters.py
@@ -0,0 +1,53 @@
+from rest_framework import serializers
+from rest_framework.relations import ManyRelatedField
+
+from drf_jsonschema.drf_jsonschema.convert import converter
+from drf_jsonschema.drf_jsonschema.converters import Error
+
+
+@converter
+class PrimaryKeyRelatedFieldConverter:
+    field_class = serializers.PrimaryKeyRelatedField
+
+    def convert(self, field):
+        return {
+            'type': 'integer',
+            'anyOf' if isinstance(field.parent, ManyRelatedField) else 'oneOf': [
+                {'const': opt.value,
+                 'title': opt.display_text}
+                for opt in field.iter_options()
+            ]
+        }
+
+        # try:
+        #     url = reverse(f'{field.queryset.model.__name__.lower()}-list')
+        #     return {
+        #         'type': 'object',
+        #         'x-fromUrl': url,
+        #         # 'x-itemsProp': '',
+        #         'x-itemTitle': 'title',
+        #         'x-itemKey': 'href'
+        #     }
+        # except NoReverseMatch:
+        #     return {
+        #         'type': 'integer'
+        #     }
+
+
+@converter
+class DecimalFieldConverter(Converter):
+    # the JSON Schema spec doesn't support decimals, I suggested it should
+    # https://github.com/json-schema-org/json-schema-spec/issues/361
+    type = 'string'
+    field_class = serializers.DecimalField
+
+    def convert(self, field):
+        # if field.max_digits is not None:
+        #     raise Error("max_digits not yet supported")
+        # who cares tbh
+        if not getattr(field, 'coerce_to_string', True):
+            raise Error("coerce_to_string must be True")
+        result = super(DecimalFieldConverter, self).convert(field)
+        result['pattern'] = "^\\-?[0-9]*(\\.[0-9]{1,%d})?$" % (
+            field.decimal_places)
+        return result
